ca29fa73b67b41848dcba605171c359a11f3ae61
BINZEO.IOT <binzeo369@gmail.com>
2026-06-26 12:35:33 +0600

    index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const app = express();
 // SUPABASE CONFIGURATION
 // ============================================
 const SUPABASE_URL = "https://kfncdapeswlnwsackkdy.supabase.co";
-const SUPABASE_KEY = "*eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmbmNkYXBlc3dsbndzYWNra2R5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMzY5NjgsImV4cCI6MjA5NTYxMjk2OH0.w0JCxkp0GHhwBboSQXYjA3lqUKEWtgbOgq07D554wK8";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmbmNkYXBlc3dsbndzYWNra2R5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwMzY5NjgsImV4cCI6MjA5NTYxMjk2OH0.w0JCxkp0GHhwBboSQXYjA3lqUKEWtgbOgq07D554wK8";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
